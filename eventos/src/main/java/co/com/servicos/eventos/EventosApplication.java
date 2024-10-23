cec4a220e211328b5ce14ca006978393d311e3bd
Chologalactico <116129679+Chologalactico@users.noreply.github.com>
2024-10-22 20:25:34 -0500

    [ADD]Sse

---
+++ eventos/src/main/java/co/com/servicos/eventos/EventosApplication.java
@@ -0,0 +1,15 @@
+package co.com.servicos.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class EventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosApplication.class, args);
+	}
+
+}
